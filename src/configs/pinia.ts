d7b59f884639e5b82ebc5908a13095b8dbfa7c03
Vida Xie <38204901+9romise@users.noreply.github.com>
2024-04-19 23:02:31 +0800

    feat: add pinia/no-duplicate-store-ids

--- src/configs/pinia.ts
+++ src/configs/pinia.ts
@@ -1,10 +1,13 @@
 import { GLOB_JS, GLOB_TS, type TypedFlatConfigItem } from '@antfu/eslint-config';
 import pluginPinia from 'eslint-plugin-pinia';
 
+const GLOB_PINIA_JS = `stores/${GLOB_JS}`;
+const GLOB_PINIA_TS = `stores/${GLOB_TS}`;
+
 export const pinia: TypedFlatConfigItem[] = [
   {
     name: 'vida/pinia',
-    files: [GLOB_JS, GLOB_TS],
+    files: [GLOB_PINIA_JS, GLOB_PINIA_TS],
     plugins: {
       pinia: pluginPinia,
     },
@@ -15,6 +18,7 @@ export const pinia: TypedFlatConfigItem[] = [
         storeSuffix: 'Store',
       }],
       'pinia/require-setup-store-properties-export': ['warn'],
+      'pinia/no-duplicate-store-ids': ['error'],
     },
   },
 ];
