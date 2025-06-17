594ca313a53985ed6a53d6787bf0befb255b9742
Vida Xie <vida_2020@163.com>
2025-06-17 21:45:38 +0800

    chore: ensure `@nuxt/eslint-plugin` is installed

--- src/configs/nuxt.ts
+++ src/configs/nuxt.ts
@@ -1,11 +1,15 @@
 import type { OptionsFiles } from '@antfu/eslint-config'
 import type { OptionsOverrides, TypedFlatConfigItem } from '../types'
-import { GLOB_SRC, interopDefault } from '@antfu/eslint-config'
+import { ensurePackages, GLOB_SRC, interopDefault } from '@antfu/eslint-config'
 
 export async function nuxt(options: OptionsFiles & OptionsOverrides = {}): Promise<TypedFlatConfigItem[]> {
   const {
     files = [GLOB_SRC],
   } = options
+
+  await ensurePackages([
+    '@nuxt/eslint-plugin',
+  ])
 
   const pluginNuxt = await interopDefault(import('@nuxt/eslint-plugin'))
 
