e55c1fd18598252201b3219369823dad05352e63
Vida Xie <vida_2020@163.com>
2025-06-20 13:37:33 +0800

    fix(style): update `quotes`

--- src/overrides/stylistic.ts
+++ src/overrides/stylistic.ts
@@ -1,6 +1,10 @@
 import type { TypedFlatConfigItem } from '@antfu/eslint-config'
 
 export const stylistic: TypedFlatConfigItem['rules'] = {
+  'style/quotes': ['error', 'single', {
+    avoidEscape: true,
+    allowTemplateLiterals: 'avoidEscape',
+  }],
   'style/arrow-parens': ['error', 'always'],
   'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
 }
