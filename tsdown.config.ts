3f2c2bf0d2404a2e66c7903e16ee6449caa48d3e
Vida Xie <vida_2020@163.com>
2025-08-22 11:21:03 +0800

    build: specific `tsconfig` in `tsdown`

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'tsdown'
 export default defineConfig([
   {
     entry: 'src/index.ts',
+    dts: {
+      tsconfig: 'tsconfig.app.json',
+    },
   },
   {
     entry: 'src/cli/index.ts',
