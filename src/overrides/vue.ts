ce30474c54293ae5bbcaab9499b291c4e982c969
Vida Xie <vida_2020@163.com>
2025-12-29 17:31:08 +0800

    fix(vue): check if vue packages exist

--- src/overrides/vue.ts
+++ src/overrides/vue.ts
@@ -1,10 +1,20 @@
 import type { OptionsConfig } from '@antfu/eslint-config'
+import { isPackageExists } from 'local-pkg'
 
-export const vue: OptionsConfig['vue'] = {
-  overrides: {
-    'vue/max-attributes-per-line': ['error', {
-      multiline: 1,
-    }],
-    'vue/prefer-use-template-ref': ['error'],
-  },
-}
+const VuePackages = [
+  'vue',
+  'nuxt',
+  'vitepress',
+  '@slidev/cli',
+]
+
+export const vue: OptionsConfig['vue'] = VuePackages.some((i) => isPackageExists(i))
+  ? {
+      overrides: {
+        'vue/max-attributes-per-line': ['error', {
+          multiline: 1,
+        }],
+        'vue/prefer-use-template-ref': ['error'],
+      },
+    }
+  : undefined
